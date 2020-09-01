69c6a9aa0a3d5860ada386fc30df08531081911e
declarative-linter-0823 <grant.shannon90@gmail.com>
2020-09-01 17:55:46 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,9 +15,9 @@ def using_flatten (array)
 end
 
 def using_delete (array, string)
-  array.delete(string.to_s)
+  array.delete(string)
 end
 
 def using_delete_at (array, integer)
-  array.delete_at(integer.to_i)
+  array.delete_at(integer)
 end
